29a1b33dcaffbefd1fdcdd8d836aef51fb96dc90
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-07 22:05:09 -0700

    🌻UPDATE: finished structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React from 'react'
+import { Box } from '@chakra-ui/react'
 
 const App = () => {
     return (
-        <div>
-            <h1>Test</h1>
-        </div>
+        <main>
+            
+        </main>
     )
 }
 
