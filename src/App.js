e2e50729ec8932f133b52542f0917acf2733953d
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-08 00:36:12 -0700

    🌻UPDATE: fixed footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from 'react'
-import { Flex, VStack, HStack, Wrap } from '@chakra-ui/react'
+import { Flex, VStack, HStack, Wrap, Text, Button, SlideFade, Box } from '@chakra-ui/react'
 import Navbar from 'components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
     return (
-        <main>
-            <Navbar />
-        </main>
+        <div>
+            <Box style={{ minHeight: '100vh' }}>
+                <Navbar />
+                <main>
+                    <Flex justifyContent='center'>
+                        <Flex flexDir='column' alignItems='center' maxW='1500px'>
+                            <Flex flexDir='column' alignItems='center' mt='10em'>
+                                <Text fontSize='38pt' fontWeight='bold' textAlign='center'>
+                                    Hangout with your friends
+                                </Text>
+                                <Text fontSize='14pt' textAlign='center'>
+                                    A place where you and your friends can meet new people in a unique way.
+                                </Text>
+                                <HStack mt='2em'>
+                                    <Button variant='primary'>
+                                        Get Started 🚀
+                                    </Button>
+                                    <Button>
+                                        Watch Video
+                                    </Button>
+                                </HStack>
+                            </Flex>
+                            <Flex flexDir='column' alignItems='center' mt='10em'>
+
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </main>
+            </Box>
+            <Footer />
+        </div>
     )
 }
 
