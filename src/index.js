78322613407880657cbe8cfd1acea648840a413c
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-07 21:49:22 -0700

    🌻UPDATE: added providers, chakraui and config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import './style.css';
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from './App.js';
+import './style.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+import App from './App.js'
+import { CoreProvider } from 'providers/CoreProvider'
+import { UserProvider } from 'providers/UserProvider'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from 'utils/theme'
 
 const Render = () => {
     return (
-        <App />
+        <ChakraProvider theme={theme}>
+            <CoreProvider>
+                <UserProvider>
+                    <App />
+                </UserProvider>
+            </CoreProvider>
+        </ChakraProvider>
     )
 }
 
