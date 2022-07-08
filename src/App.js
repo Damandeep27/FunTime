906ae37cb384133bfe6ae398480517a00a724601
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-08 00:44:11 -0700

    🌻UPDATE: added router link connectect to features button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, VStack, HStack, Wrap, Text, Button, SlideFade, Box } from '@chakra-ui/react'
+import { Flex, HStack, Text, Button, SlideFade, Box, Link } from '@chakra-ui/react'
 import Navbar from 'components/Navbar'
 import Footer from './components/Footer'
 
@@ -10,26 +10,32 @@ const App = () => {
                 <Navbar />
                 <main>
                     <Flex justifyContent='center'>
-                        <Flex flexDir='column' alignItems='center' maxW='1500px'>
-                            <Flex flexDir='column' alignItems='center' mt='10em'>
-                                <Text fontSize='38pt' fontWeight='bold' textAlign='center'>
-                                    Hangout with your friends
-                                </Text>
-                                <Text fontSize='14pt' textAlign='center'>
-                                    A place where you and your friends can meet new people in a unique way.
-                                </Text>
-                                <HStack mt='2em'>
-                                    <Button variant='primary'>
-                                        Get Started 🚀
-                                    </Button>
-                                    <Button>
-                                        Watch Video
-                                    </Button>
-                                </HStack>
-                            </Flex>
-                            <Flex flexDir='column' alignItems='center' mt='10em'>
+                        <Flex flexDir='column' alignItems='center' maxW='1500px' h='full' justifyContent='center'>
+                            <SlideFade in={true} offsetY='-20px' delay={.45}>
+                                <Flex flexDir='column' alignItems='center' mt='8em'>
+                                    <Text fontSize='38pt' fontWeight='bold' textAlign='center'>
+                                        Hangout with your friends
+                                    </Text>
+                                    <Text fontSize='14pt' textAlign='center'>
+                                        A place where you and your friends can meet new people in a unique way.
+                                    </Text>
+                                    <HStack mt='2em'>
+                                        <Button variant='primary'>
+                                            Get Started 🚀
+                                        </Button>
+                                        <Link href='https://www.youtube.com/channel/UCmKzlQCcDv-fern-Zv8vQ0w/videos' isExternal>
+                                            <Button>
+                                                Watch Video
+                                            </Button>
+                                        </Link>
+                                    </HStack>
+                                </Flex>
+                            </SlideFade>
+                            <SlideFade in={true} offsetY='-20px' delay={.45}>
+                                <Flex flexDir='column' alignItems='center' mt='10em'>
 
-                            </Flex>
+                                </Flex>
+                            </SlideFade>
                         </Flex>
                     </Flex>
                 </main>
