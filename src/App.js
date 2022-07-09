ae94a2ece8b1a6d007c7f9a2bfdba675615b24e1
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-08 19:39:51 -0700

    🌻UPDATE: added necessaru inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Flex, HStack, Text, Button, SlideFade, Box, 
-    Link, Wrap, Image, VStack, WrapItem
-} from '@chakra-ui/react'
 import Landing from 'components/pages/Landing'
 import Login from 'components/pages/Login'
 import { Routes, Route } from 'react-router-dom'
