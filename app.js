54834e869c05a633ed7b70bd49ff28f7ef25442d
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-20 20:52:26 -0700

    🌻UPDATE: small changes

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ app.use(express.json());
 app.use('/api', router);
 app.use(errorHandler);
 
-app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 // Connections
