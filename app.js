9cc6c3fc5e3adbf3c1cb4950e2ca0afd880da180
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-22 01:55:59 -0700

    🌻UPDATE: fixed folder structure

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const express = require("express");
 const cors = require('cors');
 const router = require("./routes/index.js");
 const app = express();
-const socket = require("socket.io");
+const server = require("http").Server(app);
 const { errorHandler } = require('#middlewares/errorHandler.js');
-const gamePhysics = require('./src/utils/physics.js')
+const io = require("socket.io")(server, { 
+    cors: {
+        origin: ['http://localhost:8080', 'https://fun--time.herokuapp.com']
+    }
+});
+const { addPlayer, movePlayer, sendMessage, disconnect } = require('#socket/listeners.js')(io);
 
 // Database
 const connection = require("./connection.js");
@@ -47,111 +52,17 @@ app.get('*', (req, res) => {
 connection.once('open', () => {
     console.log("[FunTime] Connected to MongoDB")
 
-    const server = app.listen(process.env.PORT || 8080, () => {
+    server.listen(process.env.PORT || 8080, () => {
         console.log(`[FunTime] listening at http://localhost:${process.env.PORT || 8080}`)
     })
-
-    const io = socket(server);
-
-    let players = {};
 
     // Web Socket Connection
     io.on('connection', (socket) => {
         console.log(`[FunTime] user <${socket.id}> connected`);
 
-        // add new player
-        socket.on('add-player', (playerData) => {
-            players[socket.id] = { ...playerData, id: socket.id };
-
-            Object.values(players).filter((pData) => pData.id !== playerData.id).forEach((curPlayerData) => {
-                socket.emit('add-player', curPlayerData);
-            })
-
-            io.emit('add-player', { ...playerData, id: socket.id });
-        })
-
-        // move a player
-        socket.on('move-player', (keyStateObj) => {
-            if (!players) return;
-
-            const { 
-                viewport: { width, height },
-                ArrowUp, ArrowDown, ArrowLeft, ArrowRight, w, s, a, d, ' ': space 
-            } = keyStateObj;
-
-            const up = ArrowUp || w || space;
-            const down = ArrowDown || s;
-            const left = ArrowLeft || a;
-            const right = ArrowRight || d;
-
-            let player = players[socket.id];
-
-            if (!player) return;
-
-            if (up && !player.isJumping) {
-                player.dY -= gamePhysics.jumpVelocity;
-                player.isJumping = true;
-            }
-            
-            if (down) {
-                player.dY += gamePhysics.jumpVelocity;
-            }
-
-            if (player.x + 20) { // < width
-                if (player.isJumping && right) {
-                    player.dX += gamePhysics.sideJumpVelocity;
-                }
-                if (right) {
-                    player.dX += gamePhysics.playerSpeed;
-                    player.direction = 0;
-                } 
-            }
-
-            if (player.x > 0) {
-                if (player.isJumping && left) {
-                    player.dX -= gamePhysics.sideJumpVelocity;
-                }
-                if (left) {
-                    player.dX -= gamePhysics.playerSpeed;
-                    player.direction = 0;
-                }
-            }
-
-            player.dY += gamePhysics.gravity;
-            player.x += player.dX;
-            player.y += player.dY;
-            player.dX *= 0.9;
-            player.dY *= 0.9;
-
-            if (player.x + player.dX < 0) {
-                player.x = 38;
-            }
-    
-            if (player.x > width) {
-                player.x = width - player.dX - 38;
-            }
-
-            if (player.y >= height - 38) {
-                player.isJumping = false;
-                player.y = height - 38;
-            }
-        })
-
-        // update all players
-        setInterval(() => {      
-            io.emit('update-players', players);
-        }, 1000 / 60);
-
-        // send chat message
-        socket.on('send-message', (messageData) => {
-            io.emit('receive-message', messageData);
-        })
-
-        // player disconnect
-        socket.on('disconnect', () => {
-            delete players[socket.id];
-            io.emit('remove-player', socket.id);
-            console.log(`[FunTime] user <${socket.id}> disconnected`);
-        });
+        socket.on('add-player', addPlayer);
+        socket.on('move-player', movePlayer);
+        socket.on('send-message', sendMessage);
+        socket.on('disconnect', disconnect);
     });
 });
--- src/utils/physics.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    groundPos: 660,
-    playerSpeed: 0.8,
-    jumpVelocity: 40,
-    sideJumpVelocity: 0.4,
-    gravity: 0.7
-}
