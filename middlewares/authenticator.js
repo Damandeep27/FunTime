726b0c18ae32b48ae0b3172c182794dc03edffeb
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-24 21:35:52 -0700

    🌻UPDATE: added comments

--- middlewares/authenticator.js
+++ middlewares/authenticator.js
@@ -2,6 +2,7 @@ const { OAuth2Client } = require('google-auth-library');
 
 const oauth2Client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
+// Authenticate routes by oauth token
 module.exports.authenticateToken = async (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
