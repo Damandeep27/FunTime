b9f65745e70063312c6047ad0ac58088d487fc25
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-07-24 18:14:03 -0700

    🌻UPDATE: added use button and default emoji

--- routes/payment/index.js
+++ routes/payment/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const { createCheckout } = require('./controller');
+const { createCheckout, getSession } = require('./controller');
 const { authenticateToken } = require('#middlewares/authenticator.js'); 
 const { 
-    CreateSessionValidator 
+    CreateSessionValidator,
+    GetSessionValidator
 } = require('#middlewares/validators.js');
 
 router.post('/createCheckout', authenticateToken, CreateSessionValidator, createCheckout);
+router.get('/getSession', authenticateToken, GetSessionValidator, getSession);
 
 module.exports = router;
